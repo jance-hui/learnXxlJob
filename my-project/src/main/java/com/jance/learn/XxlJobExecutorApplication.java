8fb1ee41c764518e1cea44d19a58610f08334cf6
liudenghui <15564835101@163.com>
2021-07-07 14:18:34 +0800

    初步提交执行器

---
+++ my-project/src/main/java/com/jance/learn/XxlJobExecutorApplication.java
@@ -0,0 +1,13 @@
+package com.jance.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxlJobExecutorApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(com.jance.learn.XxlJobExecutorApplication.class, args);
+	}
+
+}
